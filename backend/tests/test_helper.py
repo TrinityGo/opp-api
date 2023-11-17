60552a615b44bf8d4ad611314d3f351ed3ddb8e5
CocoCSforever <yijia.ma18@gmail.com>
2023-11-16 20:09:39 -0800

    Fixed old_test_helper.py, Added encryption and decrption algorithm

--- backend/tests/test_helper.py
+++ backend/tests/test_helper.py
@@ -1,122 +1,49 @@
 from models.models import Transactions
-from routers.helpers import process_transaction, validate_card, process_card
+from routers.helpers import encrypt_card_number, decrypt_card_number,process_transaction, validate_card, process_card
 from datetime import datetime
+import random
 
 
-# good_transaction_credit_card: valid number + sufficient fund
-good_transaction_credit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "4147202464191053",
-    amount = 100,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "credit_card"
-)
-
-# good_transaction_debit_card: valid number + sufficient fund
-good_transaction_debit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "4147202464191053",
-    amount = 100,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "debit_card"
-)
-
-# good_transaction_bank_account: valid number + sufficient fund
-good_transaction_bank_account = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "4147202464191053",
-    amount = 100,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "bank_account"
-)
-
-# bad_number_transaction_credit_card: invalid number
-bad_number_transaction_credit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "0",
-    amount = 100,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "credit_card"
-)
-
-# bad_fund_transaction_credit_card: valid number + insufficient fund
-bad_fund_transaction_credit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "4147202464191053",
-    amount = 10000.1,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "credit_card"
-)
-
-# bad_number_transaction_debit_card: invalid number
-bad_number_transaction_debit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "0",
-    amount = 100,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "debit_card"
-)
-
-# bad_fund_transaction_debit_card: valid number + insufficient fund
-bad_fund_transaction_debit_card = Transactions(
-    transaction_id = 55,
-    customer_id = 1,
-    merchant_id = 2,
-    encrypted_info = "4147202464191053",
-    amount = 1000000.1,
-    time_stamp = datetime.now(),
-    status = "pending",
-    payment_type = "debit_card"
-)
+credit_card = "credit_card"
+debit_card = "debit_card"
+good_card_number = "4147202464191053"
+bad_card_number = "1234"
+good_amount = 100
+bad_amount = 10000.1
 
 
 def test_process_transaction():
-    # assert process_transaction(good_transaction_credit_card) == 'approved'
-    # assert process_transaction(good_transaction_debit_card) == 'approved'
-    # assert process_transaction(bad_number_transaction_credit_card) == 'rejected'
-    
-    assert process_transaction(bad_fund_transaction_credit_card) == 'rejected'
-    assert process_transaction(bad_number_transaction_debit_card) == 'rejected'
-    assert process_transaction(bad_fund_transaction_debit_card) == 'rejected'
-    assert process_transaction(good_transaction_bank_account) == 'completed'
+    assert process_transaction(credit_card, good_card_number, good_amount) == 'approved'
+    assert process_transaction(credit_card, good_card_number, bad_amount) == 'rejected'
+    assert process_transaction(credit_card, bad_card_number, good_amount) == 'rejected'
+    assert process_transaction(credit_card, bad_card_number, bad_amount) == 'rejected'
+
+
+    assert process_transaction(debit_card, good_card_number, good_amount) == 'completed'
+    assert process_transaction(debit_card, good_card_number, bad_amount) == 'rejected'
+    assert process_transaction(debit_card, bad_card_number, good_amount) == 'rejected'
+    assert process_transaction(debit_card, bad_card_number, bad_amount) == 'rejected'
 
 
 def test_validate_card():
-    assert validate_card(good_transaction_credit_card.encrypted_info) == True
-    assert validate_card(good_transaction_debit_card.encrypted_info) == True
-    assert validate_card(good_transaction_bank_account.encrypted_info) == True
-
-    assert validate_card(bad_number_transaction_credit_card.encrypted_info) == False
-    assert validate_card(bad_fund_transaction_credit_card.encrypted_info) == True
-    assert validate_card(bad_number_transaction_debit_card.encrypted_info) == False
-    assert validate_card(bad_fund_transaction_debit_card.encrypted_info) == True
+    assert validate_card(good_card_number) == True
+    assert validate_card(bad_card_number) == True
 
 
 def test_process_card():
-    assert process_card(good_transaction_credit_card.encrypted_info, good_transaction_credit_card.amount) == True
-    assert process_card(good_transaction_debit_card.encrypted_info, good_transaction_debit_card.amount) == True
-    assert process_card(good_transaction_bank_account.encrypted_info, good_transaction_bank_account.amount) == True
-    
-    assert process_card(bad_number_transaction_credit_card.encrypted_info, bad_number_transaction_credit_card.amount) == False
-    assert process_card(bad_fund_transaction_credit_card.encrypted_info, bad_fund_transaction_credit_card.amount) == False
-    
-    assert process_card(bad_number_transaction_debit_card.encrypted_info, bad_number_transaction_debit_card.amount) == False
-    assert process_card(bad_fund_transaction_debit_card.encrypted_info, bad_fund_transaction_debit_card.amount) == False
+    assert process_card(good_card_number, good_amount) == True
+    assert process_card(good_card_number, bad_amount) == False
+    assert process_card(bad_card_number, good_amount) == False
+    assert process_card(bad_card_number, bad_amount) == False
+
+
+def test_encrypt_card_number():
+    card_number = "1234567812345678"
+    encrypted_data = encrypt_card_number(card_number)
+    decrypted_card_number = decrypt_card_number(encrypted_data)
+    assert card_number == decrypted_card_number
+
+    card_number = str(random.randrange(0, 65536))
+    encrypted_data = encrypt_card_number(card_number)
+    decrypted_card_number = decrypt_card_number(encrypted_data)
+    assert card_number == decrypted_card_number
