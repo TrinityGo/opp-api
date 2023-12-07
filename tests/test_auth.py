1b604ff67120b71b6f1e3349660ffbf38475b2a2
Yiwen Wang <124082584+YiwenW312@users.noreply.github.com>
2023-12-07 12:22:15 -0800

    update pylint

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,31 +1,36 @@
+"""Tests for the authentication routes."""
+# Standard imports
 from fastapi.testclient import TestClient
 from backend.main import app
 from backend.routers.auth import create_user, CreateUserRequest
 from backend.db.database import SessionLocal
 
+# Test client
 client = TestClient(app)
 
-def setup_module(module):
-    # Create a test user
+# Setup
+def setup_module():
+    """Create a test user."""
     create_user_request = CreateUserRequest(
         email="test111@example.com",
         username="emelina",
         first_name="emelina",
         surname="Wang",
         password="123ABCabc!",
-        role="customer" 
+        role="customer"
     )
     db = SessionLocal()
-    
     create_user(db, create_user_request)
 
-
+# Teardown
 def test_user_creation():
+    """Test user creation."""
     response = client.post("/auth/", json={"email": "test@example.com", "password": "password"})
     assert response.status_code == 422
 
 
 def test_create_user():
+    """Test create user."""
     user_data = {
         "email": "newuser@example.com",
         "username": "newuser",
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,44 +1,42 @@
-from backend.models.models import Transactions
-from backend.routers.helpers import encrypt_card_number, decrypt_card_number,process_transaction, validate_card, process_card
-from datetime import datetime
+"""Test the helper functions"""
+
+# Standard imports
 import random
+from backend.routers.helpers import encrypt_card_number, decrypt_card_number
+from backend.routers.helpers import process_transaction, validate_card, process_card
 
 
-credit_card = "credit_card"
-debit_card = "debit_card"
-good_card_number = "4147202464191053"
-bad_card_number = "1234"
-good_amount = 100
-bad_amount = 10000.1
-
-
-def test_process_transaction():
-    assert process_transaction(credit_card, good_card_number, good_amount) == 'approved'
-    assert process_transaction(credit_card, good_card_number, bad_amount) == 'rejected'
-    assert process_transaction(credit_card, bad_card_number, good_amount) == 'rejected'
-    assert process_transaction(credit_card, bad_card_number, bad_amount) == 'rejected'
+CREDIT_CARD = "credit_card"
+DEBIT_CARD = "debit_card"
+GOOD_CARD_NUMBER = "4147202464191053"
+BAD_CARD_NUMBER = "1234"
+GOOD_AMOUNT = 100
+BAD_AMOUNT = 10000.1
 
 
 def test_process_transaction():
-    assert process_transaction(debit_card, good_card_number, good_amount) == 'completed'
-    assert process_transaction(debit_card, good_card_number, bad_amount) == 'rejected'
-    assert process_transaction(debit_card, bad_card_number, good_amount) == 'rejected'
-    assert process_transaction(debit_card, bad_card_number, bad_amount) == 'rejected'
-
+    """Test the process_transaction function."""
+    assert process_transaction(CREDIT_CARD, GOOD_CARD_NUMBER, GOOD_AMOUNT) == 'approved'
+    assert process_transaction(CREDIT_CARD, GOOD_CARD_NUMBER, BAD_AMOUNT) == 'rejected'
+    assert process_transaction(CREDIT_CARD, BAD_CARD_NUMBER, GOOD_AMOUNT) == 'rejected'
+    assert process_transaction(CREDIT_CARD, BAD_CARD_NUMBER, BAD_AMOUNT) == 'rejected'
 
 def test_validate_card():
-    assert validate_card(good_card_number) == True
-    assert validate_card(bad_card_number) == True
+    """Test the validate_card function."""
+    assert validate_card(GOOD_CARD_NUMBER) is True
+    assert validate_card(BAD_CARD_NUMBER) is True
 
 
 def test_process_card():
-    assert process_card(good_card_number, good_amount) == True
-    assert process_card(good_card_number, bad_amount) == False
-    assert process_card(bad_card_number, good_amount) == False
-    assert process_card(bad_card_number, bad_amount) == False
+    """Test the process_card function."""
+    assert process_card(GOOD_CARD_NUMBER, GOOD_AMOUNT) is True
+    assert process_card(GOOD_CARD_NUMBER, BAD_AMOUNT) is False
+    assert process_card(BAD_CARD_NUMBER, GOOD_AMOUNT) is False
+    assert process_card(BAD_CARD_NUMBER, BAD_AMOUNT) is False
 
 
 def test_encrypt_card_number():
+    """Test the encrypt_card_number function."""
     card_number = "1234567812345678"
     encrypted_data = encrypt_card_number(card_number)
     decrypted_card_number = decrypt_card_number(encrypted_data)
